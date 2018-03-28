87d662c7d7f5e4aea63bdb89a745fa3727ade853
leo bang <ryanleosb@gmail.com>
2018-03-28 01:22:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 var cart = [];
 
- function setCart(newCart) {
-   cart = newCart;
- }
-
 function getCart() {
+ return cart;
+}
+
+function setCart(c) {
+  cart = c;
   return cart;
+}
+
+function addToCart(item) {
+var price = Math.floor(Math.random() * 100) + 1;
+var item_object = {};
+
+}
+
+function viewCart() {
+  // write your code here
+}
+
+function total() {
+  // write your code here
+}
+
+function removeFromCart(item) {
+  // write your code here
+}
+
+function placeOrder(cardNumber) {
+  // write your code here
 }
